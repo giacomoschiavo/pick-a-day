ed97dd2b92d603d02774c87f33a550176764364a
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-02-20 15:01:23 +0100

    added formatToDate function

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,8 @@ export const dateToFormat = (date) => {
   const day = new Date(date);
   return `${day.getDate()}/${day.getMonth() + 1}/${day.getFullYear()}`;
 };
+
+export const formatToDate = (dateString) => {
+  const [day, month, year] = dateString.split("/");
+  return new Date(`${month}/${day}&${year}`);
+};
