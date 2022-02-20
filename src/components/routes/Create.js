80c6b3474ff2b61be705e3ff0992192f6ef1d68d
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-02-20 11:26:56 +0100

    binded TextInput to Create component's state

--- src/components/routes/Create.js
+++ src/components/routes/Create.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../ui/Button";
+import { useState, useEffect } from "react";
 import Label from "../ui/Label";
 import Calendar from "react-calendar";
 import TextInput from "../ui/TextInput";
@@ -7,16 +8,22 @@ import { useNavigate } from "react-router-dom";
 import classes from "./Create.module.css";
 import "../ui/Calendar.css";
 const Create = () => {
+  const [eventName, setEventName] = useState("");
+
   const navigate = useNavigate();
 
   const navigateToVote = () => {
     navigate("/vote");
   };
 
+  useEffect(() => {
+    console.log(eventName);
+  }, [eventName]);
+
   return (
     <>
       <Label>What's the event name?</Label>
-      <TextInput />
+      <TextInput value={eventName} setValue={setEventName} />
       <Label>When?</Label>
       <Calendar />
       <Button className={classes.createButton} onClick={navigateToVote}>
--- src/components/ui/TextInput.js
+++ src/components/ui/TextInput.js
@@ -1,15 +1,24 @@
 import React from "react";
 import classes from "./TextInput.module.css";
 
-const TextInput = () => {
+const TextInput = (props) => {
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    props.setValue(e.target.value);
+  };
+
   return (
     <div className={classes.group}>
-      <input type="text" required />
+      <input
+        type="text"
+        value={props.value}
+        onChange={onChangeHandler}
+        required
+      />
       <span className={classes.highlight} />
       <span className={classes.bar} />
       <span className={classes.label}>Name</span>
     </div>
-
   );
 };
 
