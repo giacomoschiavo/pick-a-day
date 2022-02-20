be7be2766e9f0c3b3ef1ed1fa273fb917bb1a680
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-02-20 11:11:05 +0100

    added style to ResultsTab, Results page load data dinamically

--- src/components/routes/Results.js
+++ src/components/routes/Results.js
@@ -2,11 +2,56 @@ import React from "react";
 import Label from "../ui/Label";
 import ResultTab from "../ui/ResultTab";
 import classes from "./Results.module.css";
+
+const participants = ["gigi", "gigio", "luigi", "marla", "ludobecca"];
+
+const okDays = [
+  {
+    date: "10-09-22",
+  },
+  {
+    date: "11-09-22",
+  },
+];
+
+const otherDays = [
+  {
+    date: "12-09-22",
+    participants,
+  },
+  {
+    date: "13-09-22",
+    participants,
+  },
+  {
+    date: "14-09-22",
+    participants,
+  },
+  {
+    date: "15-09-22",
+    participants,
+  },
+  {
+    date: "16-09-22",
+    participants,
+  },
+];
+
 const Results = () => {
   return (
     <div className={classes.container}>
       <Label>Results</Label>
-      <ResultTab date="12-09-22" />
+      <div className={classes.resultsTab}>
+        {okDays.map((day) => (
+          <ResultTab date={day.date} success />
+        ))}
+      </div>
+      <Label>Other days</Label>
+      <div className={classes.resultsTab}>
+        {otherDays.map((day) => (
+          <ResultTab date={day.date} participants={day.participants} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/ui/ResultTab.js
+++ src/components/ui/ResultTab.js
@@ -1,17 +1,36 @@
-import React from "react";
-import { BsCheckLg } from "react-icons/bs";
+import { useState } from "react";
+import { BsHandThumbsDown, BsHandThumbsUp } from "react-icons/bs";
 import classes from "./ResultTab.module.css";
+
 const ResultTab = (props) => {
+  const [show, setShow] = useState(false);
+
+  const clicked = () => {
+    setShow((prev) => !prev);
+  };
+
   return (
-    <div className={classes.container}>
-      <div className={classes.icon}>
-        <BsCheckLg />
+    <>
+      <div className={classes.container} onClick={clicked}>
+        <div
+          className={`${classes.icon} ${
+            props.success === true && classes.success
+          }`}
+        >
+          {props.success ? <BsHandThumbsUp /> : <BsHandThumbsDown />}
+        </div>
+        <p>{props.date || "No date"}</p>
+        <div className={classes.participants}>
+          <p>9/9</p>
+        </div>
       </div>
-      <p>{props.date || "12-09-22"}</p>
-      <div className={classes.participants}>
-        <p>9/9</p>
-      </div>
-    </div>
+      {!props.success && show && (
+        <div className={classes.list}>
+          {props.participants &&
+            props.participants.map((part) => <p>{part}</p>)}
+        </div>
+      )}
+    </>
   );
 };
 
