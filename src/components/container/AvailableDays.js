c3ad25df7d6c933325b8f5799e1917167d506278
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-02-20 15:55:50 +0100

    added divideInMonths function, AvailableDays rendered dinamically

--- src/components/container/AvailableDays.js
+++ src/components/container/AvailableDays.js
@@ -2,30 +2,35 @@ import React from "react";
 import classes from "./AvailableDays.module.css";
 import Label from "../ui/Label";
 import Day from "../ui/Day";
+import { divideInMonths } from "../../utils";
 
-const AvailableDays = () => {
+const availableDates = [
+  "12/2/2022",
+  "13/2/2022",
+  "14/2/2022",
+  "17/2/2022",
+  "19/2/2022",
+  "2/3/2022",
+  "3/3/2022",
+  "4/3/2022",
+  "7/3/2022",
+  "9/3/2022",
+];
+
+const AvailableDays = (props) => {
+  const formattedDates = divideInMonths(availableDates);
   return (
     <div className={classes.container}>
-      <Label className={classes.label}>April</Label>
-      <div className={classes.days}>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-      </div>
-      <Label className={classes.label}>May</Label>
-      <div className={classes.days}>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-        <Day>1</Day>
-      </div>
+      {Object.keys(formattedDates).map((month) => (
+        <div className={classes.monthContainer} key={month}>
+          <Label className={classes.label}>{month}</Label>
+          <div className={classes.days}>
+            {formattedDates[month].map((day) => (
+              <Day key={day}>{day}</Day>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,22 @@ export const dateToFormat = (date) => {
 };
 
 export const formatToDate = (dateString) => {
+  if (typeof dateString !== "string") throw new Error("String date needed");
+  if (!dateString.split("/")) return dateString;
   const [day, month, year] = dateString.split("/");
   return new Date(`${month}/${day}&${year}`);
+};
+
+// usata solo per array di date
+export const divideInMonths = (dates) => {
+  if (!Array.isArray(dates)) throw new Error("Array of dates needed");
+  const formattedDates = dates.map(formatToDate);
+  const months = {};
+  formattedDates.forEach((date) => {
+    const month = date.toLocaleString("default", { month: "long" });
+    const capitalCaseMonth = month.charAt(0).toUpperCase() + month.slice(1);
+    if (months[capitalCaseMonth]) months[capitalCaseMonth].push(date.getDate());
+    else months[capitalCaseMonth] = [date.getDate()];
+  });
+  return months;
 };
