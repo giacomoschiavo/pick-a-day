f230ad8ee7dbc57c1d887402073195c712cb9085
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-02-23 17:20:22 +0100

    added initial route in Home, added _redirects file for Netlify

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import Button from "../ui/Button";
 const Home = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const onClickHandler = () => {
+    navigate("/create");
+  };
+
+  return (
+    <div>
+      <Button onClick={onClickHandler} value="Go to Create" />
+    </div>
+  );
 };
 
 export default Home;
