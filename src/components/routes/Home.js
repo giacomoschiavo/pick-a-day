4a2d99a1b4e5a9a72663f3cce6ed2974cb03f683
giacomoschiavo <giacomo.schiavo12@gmail.com>
2022-03-12 12:39:18 +0100

    Banner style fix

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../ui/Button";
 import Banner from "../ui/Banner";
 
 const Home = () => {
--- src/components/ui/Banner.js
+++ src/components/ui/Banner.js
@@ -4,19 +4,31 @@ import cover from "../../assets/cover.jpg";
 import { Colors } from "../../utils";
 
 const Banner = (props) => {
-
-  return <div className={classes.container}>
-    <div className={classes.coverContainer}>
-      <div className={classes.greenCover} />
-      <img src={cover} alt="cover" className={classes.cover} />
+  return (
+    <div className={classes.container}>
+      <div className={classes.coverContainer}>
+        <div className={classes.greenCover} />
+        <img src={cover} alt="cover" className={classes.cover} />
+      </div>
+      <div className={classes.overlay}>
+        <p className={classes.firstLine} style={{ color: Colors.white }}>
+          <span>Organize your events</span> <br /> <b>in seconds</b>
+        </p>
+        <p className={classes.secondLine} style={{ color: Colors.white }}>
+          Want to ask your friends where to go friday night or arrange a meeting
+          with co-workers? Create a new event, share the link and choose the
+          best day for everyone.
+        </p>
+        <div
+          className={classes.createEvent}
+          style={{ color: Colors.primary, backgroundColor: Colors.white }}
+          onClick={props.goToCreate}
+        >
+          Create event
+        </div>
+      </div>
     </div>
-    <div className={classes.overlay}>
-      <p className={classes.firstLine} style={{ color: Colors.white }}>Organize your events <br /> in seconds</p>
-      <p className={classes.secondLine} style={{ color: Colors.white }}>Want to ask your friends where to go friday night or arrange a meeting with co-workers? Create a new event, share the link and choose the best day for everyone.</p>
-      <div className={classes.createEvent} style={{ color: Colors.primary, backgroundColor: Colors.white }} onClick={props.goToCreate}>Create event</div>
-    </div>
-  </div>;
-
+  );
 };
 
 export default Banner;
