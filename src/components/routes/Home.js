262cbfea4b3d15bfcd58f4db9b9a4a41679bf89f
Enricopro <enridalberton@gmail.com>
2022-03-13 11:39:25 +0100

    Step in Home.js done

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Banner from "../ui/Banner";
+import Label from "../ui/Label";
+import classes from "./Home.module.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -11,6 +13,27 @@ const Home = () => {
   return (
     <>
       <Banner goToCreate={() => onClickHandler()} />
+      <div className={classes.instructions}>
+        <p className={classes.howItWorks}>How it works✨</p>
+        <div className={classes.tab}>
+          <div className={classes.step}>
+            <p>1</p>
+          </div>
+          <p>Choose your event name✏️</p>
+        </div>
+        <div className={classes.tab}>
+          <div className={classes.step}>
+            <p>2</p>
+          </div>
+          <p>Choose your days📅</p>
+        </div>
+        <div className={classes.tab}>
+          <div className={classes.step}>
+            <p>3</p>
+          </div>
+          <p>Share the link!🔗</p>
+        </div>
+      </div>
     </>
   );
 };
